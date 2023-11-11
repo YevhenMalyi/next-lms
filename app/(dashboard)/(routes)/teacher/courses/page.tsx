39964d9014df682df1c11949e06a5315972d3427
Yevhen Malyi <yevhen.malyi@indeema.com>
2023-11-11 22:35:27 +0200

    add table for courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,23 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { DataTable } from './_components/data-table';
+import { columns } from './_components/columns';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const courses = await db.course.findMany({
+    where: { userId },
+    orderBy: { createdAt: 'desc' },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Create Course</Button>
-      </Link>
+      <DataTable data={courses} columns={columns} />
     </div>
   );
 };
